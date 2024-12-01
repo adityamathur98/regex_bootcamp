6961d606e8f6b6c98e74092722bf5a0f9190f893
adityamathur98 <adityamathur1889@gmail.com>
2024-12-01 14:12:28 +0530

    datatypes

---
+++ script.js
@@ -0,0 +1,14 @@
+// Create Data type - let, var, const
+// var a = "Hello";
+// console.log(a);
+
+// var a = "World";
+// console.log(a);
+
+// let a = "Hellow";
+// a = "World";
+
+const a = "Hello";
+a = "World";
+
+console.assertlog(a);
